598439f374c40bce7032ee5725523bf360aee220
izanmaizan <111477612+izanmaizan@users.noreply.github.com>
2023-08-30 22:49:25 +0700

    Update App.jsx

--- Movies-React/src/App.jsx
+++ Movies-React/src/App.jsx
@@ -1,13 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
+const Movie = ({ title, poster_path, release_date, vote_average }) => (
+  <div className="Movie-wrapper">
+    <div className="Movie-title">
+      <h2>{title}</h2>
+      </div>
+    <img
+      className="Movie-image"
+      src={`${import.meta.env.VITE_REACT_APP_BASEIMGURl}${poster_path}`}
+      alt={title}
+    />
+    <div className="Movie-date">Release: {release_date}</div>
+    <div className="Movie-rate">Rating: {vote_average}</div>
+  </div>
+);
+
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      const result = await getMovieList();
+      setPopularMovies(result);
+    };
+    fetchPopularMovies();
+  }, []);
+
+  const handleSearch = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
 
   return (
-    <>
-    <h1>izan</h1>
-    </>
-  )
-}
+    <div className="App">
+      <header className="App-header">
+        <h1>Izan{"'s"} Film Fest: Showcasing the Best of Cinema</h1>
+        <input
+          placeholder="Cari film kesayangan..."
+          className="Movie-search"
+          onChange={({ target }) => handleSearch(target.value)}
+        />
+        <div className="Movie-container">
+          {popularMovies.map((movie, i) => (
+            <Movie key={i} {...movie} />
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+};
 
-export default App
+export default App;
