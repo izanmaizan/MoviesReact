b0a08137b4e04e51ca8cd1c8383484a383367ff5
izanmaizan <izanmaizan@gmail.com>
2023-09-28 15:21:47 +0700

    update dan selesai perbaikan

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Movie = ({ title, poster_path, release_date, vote_average }) => (
+  <div className="Movie-wrapper">
+    <div className="Movie-title">
+      <h2>{title}</h2>
+      </div>
+    <img
+      className="Movie-image"
+      src={`${import.meta.env.VITE_REACT_APP_BASEIMGURl}${poster_path}`}
+      alt={title}
+    />
+    <div className="Movie-date">Release: {release_date}</div>
+    <div className="Movie-rate">Rating: {vote_average}</div>
+  </div>
+);
+
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      const result = await getMovieList();
+      setPopularMovies(result);
+    };
+    fetchPopularMovies();
+  }, []);
+
+  const handleSearch = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <header className="App-header">
+        <h1>Izan{"'s"} Film Fest: Showcasing the Best of Cinema</h1>
+        <input
+          placeholder="Cari film kesayangan..."
+          className="Movie-search"
+          onChange={({ target }) => handleSearch(target.value)}
+        />
+        <div className="Movie-container">
+          {popularMovies.map((movie, i) => (
+            <Movie key={i} {...movie} />
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+};
 
-export default App
+export default App;
